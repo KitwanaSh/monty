444bb08e4baef2756fac9265f284f5d88a7f2297
KitwanaSh <rodriguekitweze@gmail.com>
2022-10-05 14:42:02 +0200

    Feat: add task pop

--- monty.h
+++ monty.h
@@ -50,5 +50,6 @@ void free_stack(stack_t *stack);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 #endif /*End Monty*/
